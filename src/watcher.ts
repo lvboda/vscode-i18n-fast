2ccc46362a0e650900ec2bdd4167925ce92f5ba8
lvboda <lvboda705@gmail.com>
2025-05-27 23:30:24 +0800

    refactor(watcher): add toRelativePattern

--- src/watcher.ts
+++ src/watcher.ts
@@ -1,7 +1,8 @@
 import * as path from 'node:path';
-import { workspace, Uri } from 'vscode';
+import { workspace, Uri, RelativePattern } from 'vscode';
 // 这里为了支持 glob 用 chokidar@3.6.0 版本
 import { watch } from 'chokidar';
+import { isString } from 'lodash';
 
 import type { GlobPattern, Disposable } from "vscode";
 import type { FSWatcher } from 'chokidar';
@@ -26,10 +27,21 @@ export default class Watcher implements Disposable {
         this.cwd = cwd;
     }
 
-    public async watch(pattern: string | GlobPattern, callback: (state: WATCH_STATE, uri: Uri) => void): Promise<Watcher> {
+    private toRelativePattern(pattern: GlobPattern) {
+        if (isString(pattern)) {
+            return new RelativePattern(this.cwd, pattern);
+        }
+
+        return pattern;
+    }
+
+    public async watch(pattern: GlobPattern, callback: (state: WATCH_STATE, uri: Uri) => void): Promise<Watcher> {
+        const rp = this.toRelativePattern(pattern);
+        const cwd = rp.baseUri?.fsPath || this.cwd;
+
         return new Promise((resolve) => {
-            this.watcher = watch(pattern.toString(), {
-                cwd: this.cwd,
+            this.watcher = watch(rp.pattern, {
+                cwd,
                 persistent: true,
                 ignoreInitial: true,
             });
@@ -38,7 +50,7 @@ export default class Watcher implements Disposable {
                 .on('add', (relativePath) => callback?.(WATCH_STATE.CREATE, Uri.file(path.join(this.cwd, relativePath))))
                 .on('change', (relativePath) => callback?.(WATCH_STATE.CHANGE, Uri.file(path.join(this.cwd, relativePath))))
                 .on('unlink', (relativePath) => callback?.(WATCH_STATE.DELETE, Uri.file(path.join(this.cwd, relativePath))))
-                .on('error', (error) => console.error(`Watcher(cwd: ${this.cwd}, pattern: ${pattern.toString()}) error:`, error))
+                .on('error', (error) => console.error(`Watcher(cwd: ${cwd}, pattern: ${rp.pattern}) error:`, error))
                 .on('ready', () => resolve(this));
         });
     }
